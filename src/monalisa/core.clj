e913483d350d512b96f3e13dd400f25afdde8117
George Armhold <armhold@gmail.com>
2013-07-17 22:17:10 -0400

    initial image display code

--- src/monalisa/core.clj
+++ src/monalisa/core.clj
@@ -1,6 +1,17 @@
 (ns monalisa.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(import '(javax.swing JFrame JLabel JTextField JButton)
+  '(java.awt.event ActionListener)
+  '(java.awt GridLayout))
+
+(defn show-image []
+  (let [panel (proxy [javax.swing.JPanel] []
+                (paintComponent [g]
+                  (proxy-super paintComponent g)
+                  (.drawImage g (.getImage
+                                  (javax.swing.ImageIcon. "src/monalisa/Rouen_Cathedrale.jpg"))
+                    0 0 (.getWidth this) (.getHeight this) nil)))]
+    (doto (javax.swing.JFrame.)
+      (.setContentPane panel)
+      (.setSize 300 300)
+      (.setVisible true))))
