8cde919d641740211002b82c6bc00ee5c9a66afb
George Armhold <armhold@gmail.com>
2013-07-19 20:04:26 -0400

    update

--- src/monalisa/core.clj
+++ src/monalisa/core.clj
@@ -6,13 +6,24 @@
   '(java.awt Color)
   '(java.awt GridLayout))
 
+(defn init-images []
+    (def reference-image (.getImage (javax.swing.ImageIcon. "src/monalisa/Rouen_Cathedrale.jpg")))
+    (def buffered-image nil)
+
+  (let [width  (/ (.getWidth reference-image) 2)
+        height (/ (.getHeight reference-image) 2)]
+
+    (def buffered-image (java.awt.image.BufferedImage. width height java.awt.image.BufferedImage/TYPE_4BYTE_ABGR))))
+
+
 (defn show-image []
   (let [panel (proxy [javax.swing.JPanel] []
                 (paintComponent [g]
+                  (println "paintComponent")
                   (proxy-super paintComponent g)
-                  (.drawImage g (.getImage
-                                  (javax.swing.ImageIcon. "src/monalisa/Rouen_Cathedrale.jpg"))
+                  (.drawImage g buffered-image
                     0 0 (.getWidth this) (.getHeight this) nil)))]
+    (def the-panel panel) ; surely this is not the proper way to set a "member" field in clojure...
     (doto (javax.swing.JFrame.)
       (.setContentPane panel)
       (.setSize 300 300)
@@ -24,8 +35,8 @@
 
 (defn random-point [image-width image-height]
   {
-    :x (rand-int image-width)
-    :y (rand-int image-height)
+    :x (double (rand-int image-width))
+    :y (double (rand-int image-height))
     }
   )
 
@@ -37,36 +48,43 @@
   )
 
 (defn random-polygons [image-width image-height]
-  (repeatedly 10 #(random-polygon image-width image-height)))
-
-(defn draw-line-to-point [path point]
-  (.moveTo path (point :x) (point :y))
-  (.lineTo path))
+  (into [] (repeatedly 100 #(random-polygon image-width image-height))))
 
 (defn path-from-polygon [polygon]
-  (let [path (. GeneralPath GeneralPath/WIND_NON_ZERO)]
-    (map draw-line-to-point path (polygon :points))
+  (let [path (GeneralPath. GeneralPath/WIND_NON_ZERO)
+        points (polygon :points)
+        starting-point (first points)
+        remaining-points (rest points)]
+
+    (.moveTo path (starting-point :x) (starting-point :y))
+
+    (println (str "got a total point count: " (count remaining-points)))
+    (doseq [point remaining-points]
+      (println (str "lineto: " (point :x) (point :y)))
+      (.lineTo path (point :x ) (point :y)))
+
     (.closePath path)
     path))
 
 (defn draw-polygon [graphics-2d polygon]
+  (println "draw-polygon")
   (.setColor graphics-2d (polygon :color))
   (.fill graphics-2d (path-from-polygon polygon)))
 
-(defn draw-polygons [buffered-image polygons]
+(defn draw-polygons
+  ([] (draw-polygons (random-polygons (.getWidth buffered-image) (.getHeight buffered-image))))
+  ([polygons]
+    (let [g2 (.getGraphics buffered-image)
+          width (.getWidth buffered-image)
+          height (.getHeight buffered-image)]
 
-  (let [g2 (.getGraphics buffered-image)
-        width (.getWidth buffered-image)
-        height (.getHeight buffered-image)]
+      (.clearRect g2 0 0 width height)
+      (doseq [polygon polygons]
+        (draw-polygon g2 polygon))
 
-    (.clearRect g2 0 0 width height)
-    (map draw-polygon buffered-image polygons))
+      (.repaint the-panel))))
 
-
-;  g2.clearRect(0, 0, w, h);
-;  for (Polygon p : getGenes()) {
-;                                 g2.setColor(p.getColor());
-;                                 g2.fill(p.getPath());
-;                                 }
-  )
-
+(defn doit []
+  (init-images)
+  (show-image)
+  (draw-polygons))
